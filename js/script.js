84b355eddde36c364e1a8c54642865c9f41017ae
Niccolo Delfino <delfino.nicco@libero.it>
2021-01-08 18:19:51 +0100

    Slide to left ok

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $('.next i.fas').click(slideRight);
+  $('.prev i.fas').click(slideLeft);
 });
 
 function slideRight() {
@@ -11,5 +12,17 @@ function slideRight() {
     $('.images img.first').addClass('active');
   }else {
     immagineAttiva.next().addClass('active');
+  }
+}
+
+function slideLeft() {
+  var immagineAttiva = $('.images img.active');
+
+  immagineAttiva.removeClass('active');
+
+  if (immagineAttiva.hasClass('first')) {
+    $('.images img.last').addClass('active');
+  }else {
+    immagineAttiva.prev().addClass('active');
   }
 }
